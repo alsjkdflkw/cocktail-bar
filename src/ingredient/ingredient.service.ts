6009b3037dae1e4620ff1b4d330a5def5eea1f5b
alsjkdflkw <289861@student.pwr.edu.pl>
2025-10-30 15:57:26 +0100

    fixed bugs

--- src/ingredient/ingredient.service.ts
+++ src/ingredient/ingredient.service.ts
@@ -9,42 +9,42 @@ import { Ingredient } from './entities/ingredient.entity';
 export class IngredientService {
   constructor(@InjectRepository(Ingredient) private ingredientRepository : Repository<Ingredient>) {}
   
-    async  create(createIngredientDto: CreateIngredientDto) {
-      const newIngredient = this.ingredientRepository.create(createIngredientDto);
-  
-      try {
-        return await this.ingredientRepository.save(newIngredient);
-      } catch (error) {
-        if (error.code === '23505') {
-          throw new ConflictException('An ingredient with this name is already added.');
-        }
-        
-        throw new InternalServerErrorException();
+  async create(createIngredientDto: CreateIngredientDto) {
+    const newIngredient = this.ingredientRepository.create(createIngredientDto);
+
+    try {
+      return await this.ingredientRepository.save(newIngredient);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('An ingredient with this name is already added.');
       }
-  
-      return this.ingredientRepository.save(newIngredient);
+      
+      throw new InternalServerErrorException();
     }
-  
-    findAll() {
-      return this.ingredientRepository.find();
+
+    return this.ingredientRepository.save(newIngredient);
+  }
+
+  findAll() {
+    return this.ingredientRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.ingredientRepository.findOneBy({id});
+  }
+
+  async update(id: number, updateIngredientDto: UpdateIngredientDto) {
+    const ingredient = await this.findOne(id);
+    return this.ingredientRepository.save({ ...ingredient, ...updateIngredientDto});
+  }
+
+  async remove(id: number) {
+    const ingredient = await this.ingredientRepository.findOneBy({ id });
+
+    if (!ingredient) {
+      throw new NotFoundException(`Ingredient with ID "${id}" was not FOUND`);
     }
-  
-    findOne(id: number) {
-      return this.ingredientRepository.findOneBy({id});
-    }
-  
-    async update(id: number, updateIngredientDto: UpdateIngredientDto) {
-      const ingredient = await this.findOne(id);
-      return this.ingredientRepository.save({ ...ingredient, ...updateIngredientDto});
-    }
-  
-    async remove(id: number) {
-      const ingredient = await this.ingredientRepository.findOneBy({ id });
-  
-      if (!ingredient) {
-        throw new NotFoundException(`Ingredient with ID "${id}" was not FOUND`);
-      }
-  
+
     return this.ingredientRepository.remove(ingredient);
   }
 }
